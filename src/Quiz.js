af3a138bc525ce41e5a4d6f4e840921decdcdc0b
Himanshu Sareen <23.himanshusareen@gmail.com>
2020-06-01 15:48:15 +0530

    Resetting the Quiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,12 +15,22 @@ class Quiz extends Component {
       return { quiz_position: state.quiz_position + 1 }
     });
   }
+  handleResetClick() {
+    this.setState({quiz_position: 1});
+  }
   render() {
     const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
     return(
       <div>
         {
-          isQuizEnd === true ? <QuizEnd /> : <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+          isQuizEnd === true ?
+          <QuizEnd
+            resetClickHandler={this.handleResetClick.bind(this)} 
+          /> :
+          <QuizQuestion
+            showNextQuestionHandler={this.showNextQuestion.bind(this)}
+            quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+          />
         }
       </div>
     );

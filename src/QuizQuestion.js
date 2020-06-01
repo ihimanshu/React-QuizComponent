a973c042b7e6eb8d547c58c9724c03ad6bf41fee
Himanshu Sareen <23.himanshusareen@gmail.com>
2020-06-01 15:26:09 +0530

    Displaying An Error Message Based on State

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton.js';
 
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      incorrectAnswer: false
+    }
+  }
   handleClick(buttonText) {
     if(buttonText === this.props.quiz_question.answer) {
       this.props.showNextQuestionHandler();
+      this.setState((state) => {
+        incorrectAnswer: state.incorrectAnswer = false
+      });
+    } else {
+      this.setState((state) => {
+        incorrectAnswer: state.incorrectAnswer = true
+      });
     }
   }
   render() {
     return(
       <main>
+        {this.state.incorrectAnswer === true ? <p className='error'>Sorry, that's not right</p> : null}
         <section>
           <p>
           {/*instruction text goes here*/}
@@ -20,7 +34,15 @@ class QuizQuestion extends Component {
           <ul>
             {/*quiz question button logic goes here*/}
             {/*<li>{ this.props.quiz_question.answer_options[0] }</li>*/}
-            {this.props.quiz_question.answer_options.map((answer_option, index) => <QuizQuestionButton key={index} button_text={answer_option} clickHandler={this.handleClick.bind(this)} />)}
+            {this.props.quiz_question.answer_options.map
+              ((answer_option, index) =>
+                <QuizQuestionButton
+                  key={index}
+                  button_text={answer_option}
+                  clickHandler={this.handleClick.bind(this)}
+                />
+              )
+            }
             {/*<QuizQuestionButton button_text={ this.props.quiz_question.answer_options[0] } />*/}
           </ul>
         </section>
